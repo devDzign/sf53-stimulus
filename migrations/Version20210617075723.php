50dfa00f098103226cb503e827d07d4631f08edf
mchabour <mourad.chabour@services.inter-invest.fr>
2021-06-24 14:41:34 +0200

    feature(submit form with stimulus)
     - ignore file

--- migrations/Version20210617075723.php
+++ migrations/Version20210617075723.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210615173703 extends AbstractMigration
+final class Version20210617075723 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,7 +21,9 @@ final class Version20210615173703 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SEQUENCE post_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE SEQUENCE solde_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE post (id INT NOT NULL, name VARCHAR(255) NOT NULL, content TEXT DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE solde (id INT NOT NULL, data1 INT DEFAULT NULL, data2 INT DEFAULT NULL, data3 INT DEFAULT NULL, PRIMARY KEY(id))');
     }
 
     public function down(Schema $schema): void
@@ -29,6 +31,8 @@ final class Version20210615173703 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP SEQUENCE post_id_seq CASCADE');
+        $this->addSql('DROP SEQUENCE solde_id_seq CASCADE');
         $this->addSql('DROP TABLE post');
+        $this->addSql('DROP TABLE solde');
     }
 }
